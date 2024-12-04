11bd2dc2a9119dfe128414a2f461e7f20555e88c
Przysiad <bartosz.podsiad@vp.pl>
2024-12-04 20:58:08 +0100

    Add files via upload

---
+++ Zadanie 4.1.js
@@ -0,0 +1,16 @@
+let skowron = true;
+    console.log(skowron)
+    if(skowron){
+        console.log(" Pies ");
+    }
+    else {
+        console.log("Spoko czlowiek");
+} 
+let samochod = false;
+    console.log(samochod)
+    if(samochod){
+        console.log(" czerwony ");
+    }
+    else {
+        console.log("to szybki");
+}   
